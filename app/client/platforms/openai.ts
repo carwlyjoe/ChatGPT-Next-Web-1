f856d6a34c747046ac43851d6cabc17e197b7bf8
carwlyjoe <130526582+carwlyjoe@users.noreply.github.com>
2023-07-22 20:55:08 +0800

    Update openai.ts

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -133,6 +133,7 @@ export class ChatGPTApi implements LLMApi {
 
               if (res.status === 401) {
                 responseTexts.push(Locale.Error.Unauthorized);
+                location.href = "/#/auth";
               }
 
               if (extraInfo) {
